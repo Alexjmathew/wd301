4cc66e001fe20c9ee47ce4e3927de226f50877f1
Alex J Mathew <131454666+Alexjmathew@users.noreply.github.com>
2025-06-06 11:39:20 +0530

    Delete hello-react directory

--- hello-react/src/header.jsx
+++
@@ -1,7 +0,0 @@
-function Header(){
-    return (<div>
-        <h1 className="text-4xl text-left font-bold">Smarter tasks</h1>
-        <h3 className="text-left"><span className="font-bold">Project</h3>
-    </div>)
-}
-export default Header;
