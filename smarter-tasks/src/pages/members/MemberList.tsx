c6c50637074d6784d19e05d764ae72e6fda887bd
Alex J Mathew <131454666+Alexjmathew@users.noreply.github.com>
2025-06-12 14:43:30 +0530

    Add files via upload

---
+++ smarter-tasks/src/pages/members/MemberList.tsx
@@ -0,0 +1,26 @@
+import React, { Suspense } from "react";
+import { fetchMembers } from "../../context/members/actions";
+import { useEffect } from "react";
+import { useMembersDispatch } from "../../context/members/context";
+const MemberListItems = React.lazy(() => import("./MemberListItems"));
+import ErrorBoundary from "../../components/ErrorBoundary";
+
+const MemberList: React.FC = () => {
+  const dispatchMembers = useMembersDispatch();
+
+  useEffect(() => {
+    fetchMembers(dispatchMembers);
+  }, []);
+
+  return (
+    <div className="grid grid-cols-4 gap-4 mt-5">
+      <ErrorBoundary>
+        <Suspense fallback={<div className="suspense-loading">Loading...</div>}>
+          <MemberListItems />
+        </Suspense>
+      </ErrorBoundary>
+    </div>
+  );
+};
+
+export default MemberList;
