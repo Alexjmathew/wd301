7a4c22750d9cc86d48b50a62baaff8cc8c34f771
Alex J Mathew <131454666+Alexjmathew@users.noreply.github.com>
2025-06-12 14:17:15 +0530

    Delete smarter-tasks directory

--- smarter-tasks/vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-// import env from '@vitejs/plugin-env';
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  // plugins: [react(), env()],
-  plugins: [react()],
-})
